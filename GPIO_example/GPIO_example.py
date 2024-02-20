2ad409f80cbf8e40402256f25a832fcf19019691
Gaebobman <standard_lee@inha.edu>
2024-02-20 16:12:07 +0900

    GPIO example

---
+++ GPIO_example/GPIO_example.py
@@ -0,0 +1,32 @@
+import Jetson.GPIO as GPIO
+import time
+
+ 
+GPIO.setmode(GPIO.BOARD)
+print("Current Mode: ", GPIO.getmode())
+
+# Switch example
+INPUT_PIN = 13
+GPIO.setup(INPUT_PIN, GPIO.IN)
+while(True):
+    print(GPIO.input(INPUT_PIN))
+    
+
+
+"""
+# On/Off example
+LED_PIN = 33
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+for i in range(5):
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    print("High ", LED_PIN)
+    time.sleep(3)
+    GPIO.output(LED_PIN, GPIO.LOW)
+    time.sleep(1)
+    print("Low", LED_PIN)
+"""
+
+GPIO.cleanup()
+
+
