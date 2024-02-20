f4a0cfdcd2d56dbb766e99897d869ebf5aaaa583
Gaebobman <standard_lee@inha.edu>
2024-02-20 13:33:40 +0900

    test

--- Pyserial_example/read_from_arduino.py
+++ Pyserial_example/read_from_arduino.py
@@ -3,13 +3,13 @@ import time
 
 PORT = '/dev/ttyACM0'
 BAUDRATE = 9600
-arduino = serial.Serial(PORT, BAUDRATE=115200, timeout=.1) 
+arduino = serial.Serial(PORT, BAUDRATE, timeout=1) 
 
 
 while True:
     # The last bit gets rid of the new-line chars
     # You can change this to whatever you want
-    data = arduino.readline()[:-2] 
+    data = arduino.readline() 
     if data:
         print(data)
     time.sleep(1) 
